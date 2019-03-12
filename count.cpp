111e49d0b68a0072b4acb83eda4f5748b18d211b
Dheeraj GSS <44668125+Dheeraj-GSS@users.noreply.github.com>
2019-03-12 22:28:10 +0530

    Number count
    This function gives the count of numbers between num1 and num2 which are divisible by num1.

---
+++ count.cpp
@@ -0,0 +1,36 @@
+/*
+OVERVIEW:  Given 2 integers (num1, num2), write a function that returns the count of numbers between the num1..num2 that are divisible by num1.
+		E.g.: count(3, 15) returns 5 (3, 6, 9, 12, 15).
+		Note : You need to consider Inclusive range
+INPUTS:  Two numbers num1, num2.
+		num1, num2 >= 0.
+
+OUTPUT: Return the count of numbers between the num1..num2 that are divisible by num1.
+
+ERROR CASES: Return -1 in error cases.
+
+NOTES:
+*/
+#include<iostream>
+using namespace std;
+int count(int num1, int num2) {
+	int counter=0, i;
+	for(i = num1;i <= num2; i++)
+	{
+		if(i % num1 == 0){
+			counter++;
+		}
+	}
+
+	return counter;
+}
+
+ int main()
+{
+	int a, b, c;
+	cout << "Enter two numbers: "<< endl;
+	cin >> a >> b;
+	c = count(a,b);
+	cout << "Counter is: " << c << endl;
+	return 0;
+}
